bc771ab0fe6e43e8c6c5e5b772f5b9ba5252dddf
Igor Isaev <igor.igorovich.isaev@gmail.com>
2020-12-14 14:55:54 +0200

    main: add slider class

--- src/endless_slider/endless_slider.js
+++ src/endless_slider/endless_slider.js
@@ -1,45 +1,77 @@
-const buttonNext = document.querySelector(".next");
-const buttonPrev = document.querySelector(".prev");
-const slider = document.querySelector(".newslider");
-let count = 0;
-
-
-const startSlide = setInterval(nextSlide, 2000);
-
-buttonNext.addEventListener("click", nextSlide);
-
-function nextSlide() {
-
-    if (count < 4) {
-        count++;
-    } else {
-        count = 0;
+class Slider {
+    constructor(imgClassName, delay) {
+        this.imgClassName = imgClassName;
+        this.count = 0;
+        this.delay = delay;
     }
 
-    slider.style.left = `${-count * 450 + "px"}`;
-}
-
-buttonPrev.addEventListener("click", prevSlide);
-
-function prevSlide() {
-
-    if (count > 0) {
-        count--;
-    } else {
-        count = 4;
+    showSlideInterval(start) {
+        this.start = setInterval(() => this.nextSlide(), this.delay);
     }
 
-    slider.style.left = `${-count * 450 + "px"}`;
+    nextSlide(imgClassName) {
+        this.count < 4
+            ? this.count++
+            : this.count = 0;
+        document.querySelector(this.imgClassName).style.left = `${-this.count * 450 + "px"}`;
+    };
+
+    prevSlide(imgClassName) {
+        this.count > 0
+            ? this.count--
+            : this.count = 4;
+        document.querySelector(this.imgClassName).style.left = `${-this.count * 450 + "px"}`;
+    };
+}
+
+class FirstSlider extends Slider {
+    constructor(imgClassName, delay, nextBtnClassName, prevBtnClassName) {
+        super(imgClassName, delay);
+        this.nextBtnClassName = nextBtnClassName;
+        this.prevBtnClassName = prevBtnClassName;
+    }
+
+    buttonNext(nextBtnClassName, imgClassName, count) {
+        document.querySelector(this.nextBtnClassName).addEventListener("click", () => this.nextSlide());
+        document.querySelector(this.nextBtnClassName).removeEventListener("click", () => this.nextSlide());
+    };
+
+    buttonPrev(prevBtnClassName, imgClassName, count) {
+        document.querySelector(this.prevBtnClassName).addEventListener("click", () => this.prevSlide());
+        document.querySelector(this.prevBtnClassName).removeEventListener("click", () => this.prevSlide());
+    };
+}
+
+class SecondSlider extends Slider {
+    constructor(imgClassName, delay, start) {
+        super(imgClassName, delay);
+    }
+
+    addMouseEvent(imgClassName, start) {
+        let view = 0;
+        if (!view) {
+            this.showSlideInterval(start);
+            view++
+        }
+        document.querySelector(this.imgClassName).addEventListener("mouseout", () => this.showSlideInterval());
+        document.querySelector(this.imgClassName).removeEventListener("mouseout", () => this.showSlideInterval());
+    };
+
+    deleteMouseEvent(imgClassName, start) {
+        document.querySelector(this.imgClassName).addEventListener("mouseover", () => clearTimeout(this.start));
+        document.querySelector(this.imgClassName).removeEventListener("mouseover", () => clearTimeout(this.start));
+    };
 }
 
 
-slider.addEventListener("mouseover", stopWorkSlide);
-slider.addEventListener("mouseout", startWorkslide);
+let slide1 = new FirstSlider(".newslider1", 2000, ".next", ".prev");
+let slide2 = new SecondSlider(".newslider2", 2000);
 
-function stopWorkSlide() {
-    startSlide = clearInterval(startSlide);
-}
+slide1.buttonNext();
+slide1.buttonPrev();
+slide1.showSlideInterval();
+slide2.addMouseEvent();
+slide2.deleteMouseEvent();
 
-function startWorkslide() {
-    startSlide = setInterval(nextSlide, 2000);
-}
+
+
--- src/map/map.js
+++ src/map/map.js
@@ -2,7 +2,8 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.querySelector(".map"), {
-        center: { lat: 48.46612, lng: 35.05568 },
+        center: {lat: 48.46612, lng: 35.05568},
         zoom: 18,
     });
 }
+
